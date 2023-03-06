bd6bdb631b6b717808e4a20a14655720932ea321
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-06 22:26:24 +0900

    🚧 APIを叩いてみる
    APIを叩くための実験用で準備したファイル

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
