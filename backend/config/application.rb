56dc85c5a6926489feda358b6af5b84cdd6997e3
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-06 18:52:26 +0900

    🐛 セッションを有効にする
    Rails APIモードだとomniauth導入時にOmniAuth::NoSessionError が出るため対処した

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,6 +23,10 @@ module OutdoorHeartSutraApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
