019df5f954bfb1324b7bb15ac3d754dbca8e8744
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-07 23:01:55 +0900

    🚧 sutras, photosのコントローラの雛形を生成する

---
+++ backend/app/controllers/api/v1/photos_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class PhotosController < ApplicationController
+      def index
+      end
+
+      def show
+      end
+
+      def new
+      end
+
+      def create
+      end
+
+      def edit
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+    end
+  end
+end
