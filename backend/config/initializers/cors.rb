62bde819154d96399529a0a5d5da98e8c5176ff2
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-08 15:39:17 +0900

    :wrench: rack-corsの設定を変更
    originsのポート番号を変更した

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:4000"
 
     resource "*",
             headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    # credentials : true
+            methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            credentials: true
   end
 end
