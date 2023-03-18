6f465a17fd38b54eddeb9db4c8f018c4050c84e1
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-18 14:34:31 +0900

    📦 tailwindcssをインストール

--- frontend_second/src/pages/index.tsx
+++ frontend_second/src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.main}>
+        <h1 className='text-3xl font-bold underline'>Hel world!</h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
