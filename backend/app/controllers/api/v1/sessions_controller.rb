f066850054137010034bf31de886bcc61fcbe83a
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-25 21:15:14 +0900

    :sparkles: User登録、User削除のルーティングとコントローラを設定する

---
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      # create, destroyアクションに対して、CSRF保護を無効にする
+      skip_before_action :verify_authenticity_token, only: %i[create destroy]
+
+      def create
+        # 引数の条件に該当するデータがあればそれを返す。なければ新規作成する
+        user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
+        if user.save
+          redirect_to "http://localhost:4000/"
+        else
+          render json: { error: "ログインに失敗しました" }, status: :unprocessable_entity
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: :internal_server_error
+      end
+
+      def destroy
+        user = User.find_by(email: params[:email])
+        if user
+          user.destroy
+        else
+          render json: { error: "ユーザーが見つかりませんでした" }, status: :not_found
+        end
+      rescue StandardError => e
+        render json: { error: e.message }, status: :internal_server_error
+      end
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
       resources :photos
     end
   end
-  post 'auth/:provider/callback', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  post 'auth/:provider/callback', to: 'api/v1/sessions#create'
+  delete "users/:email", to: "api/v1/sessions#destroy", constraints: { email: /[^\/]+/ }
 end
