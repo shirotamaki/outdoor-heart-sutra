8b39bd74a00348a2c6c49ea096d86aecbd9ec890
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-26 00:29:15 +0900

    🔥 重複して存在していたfrontendディレクトリを削除した

--- frontend_second/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { SessionProvider } from 'next-auth/react'
-
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
-}
