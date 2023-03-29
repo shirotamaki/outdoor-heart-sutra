d1514512424130e0d1a2376764581e9bea703d3b
shirotamaki <shiro.tamaki63@gmail.com>
2023-03-29 22:16:21 +0900

    🗃️ バリデーションを追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_many :photos, dependent: :destroy
   validates :provider, presence: true
   validates :uid, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 256 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 256 }
 end
