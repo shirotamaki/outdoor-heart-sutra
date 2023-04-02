58e71a8383801d44209f1fd4302787dfe0b09c81
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-02 17:33:47 +0900

    ✨ デバイスのカメラを起動させ写真を撮影する機能を実装する

---
+++ frontend/src/components/WebcamComponent.tsx
@@ -0,0 +1,67 @@
+import Webcam from 'react-webcam'
+import { useRef, useState, useCallback } from 'react'
+
+const videoConstraints = {
+  width: 360,
+  height: 360,
+  // "user"を指定すればフロントカメラ、{ exact: "environment" }ならリアカメラ
+  facingMode:{ ideal: 'user' },
+}
+
+const WebcamComponent = () => {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(true)
+  const webcamRef = useRef<Webcam>(null)
+  const [url, setUrl] = useState<string | null>(null)
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot()
+    if (imageSrc) {
+      setUrl(imageSrc)
+    }
+  }, [webcamRef])
+
+  return (
+    <>
+      <h1>撮影モード</h1>
+      {isCaptureEnable && (
+        <>
+          <div>
+            <Webcam
+              audio={false}
+              width={360}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat='image/jpeg'
+              videoConstraints={videoConstraints}
+              playsInline
+            />
+          </div>
+          <button
+            onClick={() => {
+              capture(), setCaptureEnable(false)
+            }}
+          >
+            撮影する
+          </button>
+        </>
+      )}
+      {url && (
+        <>
+          <div>
+            <img src={url} alt='Screenshot' />
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                setUrl(null), setCaptureEnable(true)
+              }}
+            >
+              削除する
+            </button>
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default WebcamComponent
