86b66a8cac62b90b046760cb11e9b7af72fe39d9
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-03 18:42:21 +0900

    🔧 外部画像のホスト名をホワイトリストに追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 }
 
 module.exports = nextConfig
