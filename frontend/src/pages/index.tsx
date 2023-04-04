9d1bd1bfeaf1e5d83b95cce2bf93ef17aceb2df0
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-04 22:55:15 +0900

    🐛 デバッグのためコメントアウトする

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,58 +1,58 @@
-import axios from 'axios'
-import Head from 'next/head'
-import Link from 'next/link'
+// import axios from 'axios'
+// import Head from 'next/head'
+// import Link from 'next/link'
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
-interface Sutra {
-  id: number
-  kanji: string
-}
+// interface Sutra {
+//   id: number
+//   kanji: string
+// }
 
-interface HomeProps {
-  sutras: Sutra[]
-}
+// interface HomeProps {
+//   sutras: Sutra[]
+// }
 
-export default function Home({ sutras }: HomeProps) {
-  return (
-    <>
-      <Head>
-        <title>アウトドア般若心経 | Top</title>
-      </Head>
-      <div>
-        <h1>アウトドア般若心経</h1>
-        <p>自分探しならぬ、自分なくしの旅へ</p>
-      </div>
-      <div>
-        <Link href='/welcome'>welcomeページへ</Link>
-      </div>
-      <div>
-        <Sutra sutras={sutras} />
-      </div>
-    </>
-  )
-}
+// export default function Home({ sutras }: HomeProps) {
+//   return (
+//     <>
+//       <Head>
+//         <title>アウトドア般若心経 | Top</title>
+//       </Head>
+//       <div>
+//         <h1>アウトドア般若心経</h1>
+//         <p>自分探しならぬ、自分なくしの旅へ</p>
+//       </div>
+//       <div>
+//         <Link href='/welcome'>welcomeページへ</Link>
+//       </div>
+//       {/* <div> */}
+//         {/* <Sutra sutras={sutras} /> */}
+//       {/* </div> */}
+//     </>
+//   )
+// }
 
-function Sutra({ sutras }: HomeProps) {
-  return (
-    <div>
-      {sutras.map((sutra, index) => (
-        <ul key={index}>
-          <li>{sutra.kanji}</li>
-        </ul>
-      ))}
-    </div>
-  )
-}
+// function Sutra({ sutras }: HomeProps) {
+//   return (
+//     <div>
+//       {sutras.map((sutra, index) => (
+//         <ul key={index}>
+//           <li>{sutra.kanji}</li>
+//         </ul>
+//       ))}
+//     </div>
+//   )
+// }
 
-export async function getStaticProps() {
-  const response = await axios.get(`${apiUrl}/api/v1/sutras`)
-  const sutras = response.data
+// export async function getStaticProps() {
+//   const response = await axios.get(`${apiUrl}/api/v1/sutras`)
+//   const sutras = response.data
 
-  return {
-    props: {
-      sutras,
-    },
-    revalidate: 10, // オプションでキャッシュの更新間隔を指定
-  }
-}
+//   return {
+//     props: {
+//       sutras,
+//     },
+//     revalidate: 10, // オプションでキャッシュの更新間隔を指定
+//   }
+// }
