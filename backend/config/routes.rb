c36e942af9dc79d17202502561e3f88898783b3e
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-13 22:24:22 +0900

    ♻️ ルーティングのurlを変更

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:create]
       resources :sutras
       resources :photos
     end
   end
-  post 'auth/:provider/callback', to: 'api/v1/users#create'
   delete "users/:email", to: "api/v1/users#destroy", constraints: { email: /[^\/]+/ }
 end
