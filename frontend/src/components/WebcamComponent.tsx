190d2fcd5fb1129a9ca1d80701052e3b41c43cc4
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-14 20:38:28 +0900

    ✨ カメラのフロントとリアを切り替える

--- frontend/src/components/WebcamComponent.tsx
+++ frontend/src/components/WebcamComponent.tsx
@@ -2,23 +2,33 @@ import Image from 'next/image'
 import { useRef, useState, useCallback } from 'react'
 import Webcam from 'react-webcam'
 
-const videoConstraints = {
-  width: 360,
-  height: 360,
-  // "user"を指定すればフロントカメラ、{ exact: "environment" }ならリアカメラ
-  facingMode: { ideal: 'user' },
-}
+type FacingModeType = string | { exact: string }
+
+const frontCamera: FacingModeType = 'user'
+const rearCamera: FacingModeType = { exact: 'environment' }
 
 const WebcamComponent = () => {
   const [isCaptureEnable, setCaptureEnable] = useState<boolean>(true)
   const webcamRef = useRef<Webcam>(null)
   const [url, setUrl] = useState<string | null>(null)
+  const [facingMode, setFacingMode] = useState<FacingModeType>(rearCamera)
+
+  const videoConstraints = {
+    width: 360,
+    height: 360,
+    facingMode: facingMode,
+  }
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot()
     if (imageSrc) {
       setUrl(imageSrc)
     }
   }, [webcamRef])
+
+  const toggleCamera = () => {
+    setFacingMode((prev) => (prev === frontCamera ? rearCamera : frontCamera))
+  }
 
   return (
     <>
@@ -44,6 +54,7 @@ const WebcamComponent = () => {
           >
             撮影する
           </button>
+          <button onClick={toggleCamera}>カメラを切り替える</button>
         </>
       )}
       {url && (
