a1e01096a983a09aeeebe26da5e5f298497d995f
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-24 23:10:17 +0900

    🔧 geocodingApiUrlを設定する

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -3,3 +3,4 @@ export const clientSecret = process.env.GOOGLE_CLIENT_SECRET as string
 export const railsApiUrl = process.env.NEXT_PUBLIC_RAILS_API_URL as string
 export const mapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string
 export const nextSecret = process.env.NEXT_SECRET as string
+export const geocodingApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
