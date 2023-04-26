1974657c9694c07224c7a539006519fb68209356
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-26 17:54:15 +0900

    🗃️ 開発、テスト、本番環境用にsutrasテーブルのkanjiデータを投入する

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,6 +2,9 @@
 
 require 'csv'
 
-CSV.foreach('db/sutra.csv') do |row|
-  Sutra.create(kanji: row[0])
+seeds_config = Rails.application.config_for(:seeds)
+sutras_csv_path = seeds_config["sutras_csv_path"]
+
+CSV.foreach(sutras_csv_path) do |row|
+  Sutra.create!(kanji: row[0])
 end
