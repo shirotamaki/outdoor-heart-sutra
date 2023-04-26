981ca2516a0e4f72e870528fc302736786b00e65
shirotamaki <shiro.tamaki63@gmail.com>
2023-04-26 20:57:18 +0900

    ✨ showページのルーティングとコントローラを設定する

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :sutras
+      resources :sutras, only: [:index, :show]
       resources :photos
     end
   end
