fa63527344d7946a905fafb62f659ea4f45a7aba
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-15 22:24:51 +0900

    :cop: Fix rubocop warning

--- backend/app/controllers/api/v1/photos_controller.rb
+++ backend/app/controllers/api/v1/photos_controller.rb
@@ -4,14 +4,14 @@ module Api
   module V1
     class PhotosController < ApplicationController
       skip_before_action :verify_authenticity_token
-      before_action :set_photo, only: [:show, :update, :destroy]
+      before_action :set_photo, only: %i[show update destroy]
 
       def find_photo
         photo = Photo.find_by(sutra_id: params[:sutraId], user_id: params[:userId])
         if photo
           render json: { photo_id: photo.id }
         else
-          render json: { error: "写真が見つかりませんでした" }, status: :not_found
+          render json: { error: '写真が見つかりませんでした' }, status: :not_found
         end
       end
 
@@ -25,10 +25,10 @@ module Api
         if photo.save
           head :ok
         else
-          render json: { error: "保存に失敗しました" }, status: :unprocessable_entity
+          render json: { error: '保存に失敗しました' }, status: :unprocessable_entity
         end
       rescue StandardError => e
-        render json: { error: "保存に失敗しました", errors: photo.errors }, status: :internal_server_error
+        render json: { error: '保存に失敗しました', errors: e.message }, status: :internal_server_error
       end
 
       def show
@@ -39,17 +39,17 @@ module Api
         if @photo.update(photo_params)
           head :ok
         else
-          render json: { error: "更新に失敗しました" }, status: :unprocessable_entity
+          render json: { error: '更新に失敗しました' }, status: :unprocessable_entity
         end
       rescue StandardError => e
-        render json: { error: "更新に失敗しました", details: e.message }, status: :internal_server_error
+        render json: { error: '更新に失敗しました', details: e.message }, status: :internal_server_error
       end
 
       def destroy
         @photo.destroy
         head :ok
       rescue StandardError => e
-        render json: { error: "削除に失敗しました", details: e.message }, status: :internal_server_error
+        render json: { error: '削除に失敗しました', details: e.message }, status: :internal_server_error
       end
 
       private
@@ -66,7 +66,7 @@ module Api
           longitude: params[:longitudeData],
           address: params[:addressData],
           user_id: params[:currentUserId],
-          sutra_id: params[:currentSutraId],
+          sutra_id: params[:currentSutraId]
         }.compact
       end
     end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require "csv"
+require 'csv'
 
 begin
   seeds_config = Rails.application.config_for(:sutras_seeds)
-  sutras_csv_path = seeds_config["sutras_csv_path"]
+  sutras_csv_path = seeds_config['sutras_csv_path']
 
   CSV.foreach(sutras_csv_path) do |row|
     Sutra.create!(kanji: row[0])
   end
 rescue StandardError => e
-  puts "Error occurred while populating sutras: #{e}"
+  Rails.logger.debug "Error occurred while populating sutras: #{e}"
 end
