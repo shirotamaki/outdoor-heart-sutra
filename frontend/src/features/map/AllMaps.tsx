e37583824326cd8986b32ee2305bbe855f70f1c3
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-15 22:10:33 +0900

    ✨ DBに保存している全ての位置情報を取得しGoogle Mapsにマーカーを立てる

---
+++ frontend/src/features/map/AllMaps.tsx
@@ -0,0 +1,51 @@
+import { GoogleMap, LoadScriptNext, MarkerF } from '@react-google-maps/api'
+import { useEffect, useState } from 'react'
+import { mapsApiKey } from '@/config/index'
+
+const containerStyle = {
+  width: '360px',
+  height: '360px',
+}
+
+const center = {
+  lat: 35.693905085681585,
+  lng: 139.74912823866393,
+}
+
+type MarkerLocation = {
+  lat: number
+  lng: number
+}
+
+type AllMapsProps = {
+  markerLocations: MarkerLocation[]
+}
+
+const AllMaps = ({ markerLocations }: AllMapsProps) => {
+  const [map, setMap] = useState<google.maps.Map | null>(null)
+
+  const handleMapLoad = (mapInstance: google.maps.Map) => {
+    setMap(mapInstance)
+  }
+
+  useEffect(() => {
+    if (map && markerLocations.length > 0) {
+      map.panTo(markerLocations[0])
+    }
+  }, [map, markerLocations])
+
+  if (!mapsApiKey) {
+    return <div>Google Maps APIキーが設定されていません</div>
+  }
+  return (
+    <LoadScriptNext googleMapsApiKey={mapsApiKey}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={8} onLoad={handleMapLoad}>
+        {markerLocations.map((location, index) => (
+          <MarkerF key={index} position={location} />
+        ))}
+      </GoogleMap>
+    </LoadScriptNext>
+  )
+}
+
+export default AllMaps
