52b46b95fa410905c591baee37d706cddcc78f20
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-24 15:35:00 +0900

    🩹 設定値を変更する

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/features/**/*.{js,ts,jsx,tsx}', '/src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/features/**/*.{js,ts,jsx,tsx}', '/src/components/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
       gold: '#CAB54F', // 仏像カラー
-      gray: '#F2F2F2', // 背景カラー
+      gray: '#eaeaea', // 背景カラー
       tetsuguro: '#1D1506', //てつぐろ（フォント用）
     },
     extend: {},
