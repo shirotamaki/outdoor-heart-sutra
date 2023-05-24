b05878093e976397d3e9b2f1b98bdc4a38b30ce9
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-24 14:40:02 +0900

    ♻️ head, header, footer要素をコンポーネント化する

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import HomeButton from '@/components/HomeButton'
+import Logout from '@/features/auth/Logout'
+import DeleteUser from '@/features/user/DeleteUser'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <HomeButton />
+      </div>
+      <div>
+        <Link href='/about'>アウトドア般若心経とは</Link>
+      </div>
+      <div>
+        <Link href='/maps'>全体地図</Link>
+      </div>
+      <div>
+        <Logout />
+      </div>
+      <div>
+        <DeleteUser />
+      </div>
+    </header>
+  )
+}
+
+export default Header
