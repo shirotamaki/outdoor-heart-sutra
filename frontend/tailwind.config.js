cefce1d97ffb563623958101d52226ca13a74a5f
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-26 21:34:10 +0900

    🩹 タイポを修正、設定を変更

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,15 +3,16 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/features/**/*.{js,ts,jsx,tsx}',
-    '/src/components/*.{js,ts,jsx,tsx}',
+    './src/components/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      gold: '#CAB54F', // 仏像カラー
-      gray: '#eaeaea', // 背景カラー
-      tetsuguro: '#1D1506', //てつぐろ（フォント用）
+    extend: {
+      colors: {
+        gold: '#CAB54F', // 仏像カラー
+        gray: '#d0cbcb', // 背景カラー
+        tetsuguro: '#1D1506', //てつぐろ（フォント用）
+      },
     },
-    extend: {},
-    plugins: [],
   },
+  plugins: [],
 }
