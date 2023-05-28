d59c7e625f898d0b43589f15954bb03869d872be
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-28 21:17:29 +0900

    ⚡️ useExifを使用し位置情報を取得する

--- frontend/src/features/photo/PhotoUploadAndPreview.tsx
+++ frontend/src/features/photo/PhotoUploadAndPreview.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
+import useCurrentLocation from '@/hooks/useCurrentLocation'
+import useExif from '@/hooks/useExif'
 
 const PhotoUploadAndPreview = () => {
   const [previewUrl, setPreviewUrl] = useState<string | null>(null)
+  const { exifLocation, fetchExif } = useExif()
+  const { currentLocation, getCurrentLocation } = useCurrentLocation()
 
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files !== null) {
       const file = event.target.files[0]
+
+      await fetchExif(file)
 
       try {
         const heic2any = (await import('heic2any')).default
@@ -14,7 +20,7 @@ const PhotoUploadAndPreview = () => {
           const convertedImage = await heic2any({
             blob: file,
             toType: 'image/jpeg',
-            quality: 0.5,
+            quality: 0.2,
           })
           if (Array.isArray(convertedImage)) {
             throw new Error('Unexpected multiple blobs')
@@ -31,17 +37,23 @@ const PhotoUploadAndPreview = () => {
     }
   }
 
+  useEffect(() => {
+    if (!exifLocation) {
+      getCurrentLocation()
+    }
+  }, [exifLocation, getCurrentLocation])
+
+  const location = exifLocation || currentLocation
+
   return (
     <div>
-      <input
-        type='file'
-        accept='image/jpeg, image/png, image/heic'
-        onChange={handleFileChange}
-      />
-      {previewUrl ? (
-        <Image src={previewUrl} alt='preview' width={480} height={480} />
-      ) : (
-        'No preview'
+      <input type='file' accept='image/jpeg, image/png, image/heic' onChange={handleFileChange} />
+      {previewUrl && <Image src={previewUrl} alt='preview' width={480} height={480} />}
+      {location && (
+        <div>
+          <p>Latitude: {location.lat}</p>
+          <p>Longitude: {location.lng}</p>
+        </div>
       )}
     </div>
   )
