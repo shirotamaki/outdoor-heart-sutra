e13f7d6046b84f6cdf955c86c398de6d55802642
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-31 19:59:16 +0900

    🩹 modelのvalidatesを修正する

--- backend/app/models/photo.rb
+++ backend/app/models/photo.rb
@@ -3,6 +3,7 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :sutra
-  validates :photo_data, presence: true
+  validates :imageUrl, presence: true
+  validates :croppedImageUrl, presence: true
   validates :note, length: { maximum: 256 }
 end
