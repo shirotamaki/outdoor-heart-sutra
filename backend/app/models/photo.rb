1a6d4b4c87a558dd15b5e160847a0aea22839b7f
shirotamaki <shiro.tamaki63@gmail.com>
2023-05-31 20:27:56 +0900

    🩹 カラム名を修正する

--- backend/app/models/photo.rb
+++ backend/app/models/photo.rb
@@ -3,7 +3,7 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :sutra
-  validates :imageUrl, presence: true
-  validates :croppedImageUrl, presence: true
+  validates :image_url, presence: true
+  validates :cropped_image_url, presence: true
   validates :note, length: { maximum: 256 }
 end
