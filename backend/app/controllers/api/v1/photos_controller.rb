52fc59a18c707447ffff20bb2d38a2ca3b429df5
shirotamaki <shiro.tamaki63@gmail.com>
2023-06-07 10:48:01 +0900

    🗃️ image、corpped_imageを使用するためにコントローラを修正する
    image、corpped_imageをストレージに保存、呼び出し、上書き保存、削除するためにコントローラを修正する

--- backend/app/controllers/api/v1/photos_controller.rb
+++ backend/app/controllers/api/v1/photos_controller.rb
@@ -11,7 +11,7 @@ module Api
         if photo
           render json: { photo_id: photo.id }
         else
-          render json: { error: '写真が見つかりませんでした' }, status: :not_found
+          render json: { error: "写真が見つかりませんでした" }, status: :not_found
         end
       end
 
@@ -19,42 +19,60 @@ module Api
         user = User.find_by(id: params[:user_id])
 
         if user.nil?
-          render json: { error: 'ユーザーが見つかりません' }, status: :not_found
+          render json: { error: "ユーザーが見つかりません" }, status: :not_found
         else
-          render json: user.photos
+          photos = user.photos.map do |photo|
+            image_url = rails_blob_url(photo.image) if photo.image.attached?
+            cropped_image_url = rails_blob_url(photo.cropped_image) if photo.cropped_image.attached?
+
+            photo.as_json.merge(image_url: image_url, cropped_image_url: cropped_image_url)
+          end
+          render json: photos
         end
       end
 
       def create
         photo = Photo.new(photo_params)
+
+        photo.image.attach(params[:image])
+        photo.cropped_image.attach(params[:croppedImage])
+
         if photo.save
           head :ok
         else
-          render json: { error: '保存に失敗しました' }, status: :unprocessable_entity
+          render json: { error: "保存に失敗しました" }, status: :unprocessable_entity
         end
       rescue StandardError => e
-        render json: { error: '保存に失敗しました', errors: e.message }, status: :internal_server_error
+        render json: { error: "保存に失敗しました", errors: e.message }, status: :internal_server_error
       end
 
       def show
-        render json: @photo
+        image_url = rails_blob_url(@photo.image) if @photo.image.attached?
+        cropped_image_url = rails_blob_url(@photo.cropped_image) if @photo.cropped_image.attached?
+        render json: @photo.as_json.merge(image_url: image_url, cropped_image_url: cropped_image_url)
       end
 
       def update
         if @photo.update(photo_params)
+          @photo.image.attach(params[:image]) if params[:image]
+          @photo.cropped_image.attach(params[:croppedImage]) if params[:croppedImage]
+
           head :ok
         else
-          render json: { error: '更新に失敗しました' }, status: :unprocessable_entity
+          render json: { error: "更新に失敗しました" }, status: :unprocessable_entity
         end
       rescue StandardError => e
-        render json: { error: '更新に失敗しました', details: e.message }, status: :internal_server_error
+        render json: { error: "更新に失敗しました", details: e.message }, status: :internal_server_error
       end
 
       def destroy
+        @photo.image.purge if @photo.image.attached?
+        @photo.cropped_image.purge if @photo.cropped_image.attached?
+
         @photo.destroy
         head :no_content
       rescue StandardError => e
-        render json: { error: '削除に失敗しました', details: e.message }, status: :internal_server_error
+        render json: { error: "削除に失敗しました", details: e.message }, status: :internal_server_error
       end
 
       private
@@ -65,14 +83,12 @@ module Api
 
       def photo_params
         {
-          image_url: params[:imageUrl],
-          cropped_image_url: params[:croppedImageUrl],
           note: params[:noteData],
           latitude: params[:latitudeData],
           longitude: params[:longitudeData],
           address: params[:addressData],
           user_id: params[:currentUserId],
-          sutra_id: params[:currentSutraId]
+          sutra_id: params[:currentSutraId],
         }.compact
       end
     end
