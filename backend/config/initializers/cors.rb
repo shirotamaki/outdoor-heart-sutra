8fa74db7dd394b648737c3e638a63c9014b2c3d4
shirotamaki <shiro.tamaki63@gmail.com>
2023-06-16 21:05:08 +0900

    🐛 デバッグ用で追加する

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
     origins Rails.application.credentials.next_url
 
