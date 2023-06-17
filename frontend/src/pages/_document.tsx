afe74599d9d7896b90fa1689bee2a40062d75487
shirotamaki <shiro.tamaki63@gmail.com>
2023-06-17 20:00:45 +0900

    💄 GoogleFontsを設定する

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang='en'>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com'></link>
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous'></link>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Noto+Serif:wght@100&family=Yuji+Syuku&display=swap'
+            rel='stylesheet'
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
