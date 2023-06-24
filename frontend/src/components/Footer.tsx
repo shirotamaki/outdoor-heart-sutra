ee39594166d27adf4bb8aaf5edfe965063944dc7
shirotamaki <shiro.tamaki63@gmail.com>
2023-06-24 23:37:06 +0900

    💄 フッターの配色を変更する

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
 import GitHubLink from '@/components/GitHubLink'
 import TwitterLink from '@/components/TwitterLink'
-import DeleteUser from '@/features/user/DeleteUser'
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col justify-center items-center bg-white font-notoSans py-4'>
+    <footer className='flex flex-col justify-center items-center bg-tetsuguro font-notoSans text-white py-2'>
       <div className='flex justify-center items-center space-x-4 mb-4'>
         <Link href='/terms-of-service' className='hover:opacity-50 transition-all duration-100'>
           利用規約
