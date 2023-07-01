23cf20585be13c316283b490ff43d708dbad7c7b
shirotamaki <shiro.tamaki63@gmail.com>
2023-07-01 22:50:27 +0900

    ♻️ shoulda-matchersを使って書き換える

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -3,56 +3,20 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  subject { FactoryBot.create(:user) }
+
   describe "validations" do
-    it "validates presence of provider" do
-      user = FactoryBot.build(:user, provider: nil)
-      expect(user).not_to be_valid
-    end
-
-    it "validates presence of uid" do
-      user = FactoryBot.build(:user, uid: nil)
-      expect(user).not_to be_valid
-    end
-
-    it "validates uniqueness of uid" do
-      FactoryBot.create(:user, uid: "test_uid")
-      user = FactoryBot.build(:user, uid: "test_uid")
-      expect(user).not_to be_valid
-    end
-
-    it "validates presence of name" do
-      user = FactoryBot.build(:user, name: nil)
-      expect(user).not_to be_valid
-    end
-
-    it "validates length of name" do
-      user = FactoryBot.build(:user, name: "a" * 257)
-      expect(user).not_to be_valid
-    end
-
-    it "validates presence of email" do
-      user = FactoryBot.build(:user, email: nil)
-      expect(user).not_to be_valid
-    end
-
-    it "validates uniqueness of email" do
-      FactoryBot.create(:user, email: "test@example.com")
-      user = FactoryBot.build(:user, email: "test@example.com")
-      expect(user).not_to be_valid
-    end
-
-    it "validates length of email" do
-      user = FactoryBot.build(:user, email: "a" * 257 )
-      expect(user).not_to be_valid
-    end
+    it { should validate_presence_of(:provider) }
+    it { should validate_presence_of(:uid) }
+    it { should validate_uniqueness_of(:uid) }
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(256) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_length_of(:email).is_at_most(256) }
   end
 
   describe "associations" do
-    it "has many photos" do
-      user = FactoryBot.create(:user)
-      FactoryBot.create(:photo, user: user)
-      FactoryBot.create(:photo, user: user)
-      expect(user.photos.count).to eq(2)
-    end
+    it { should have_many(:photos) }
   end
 end
