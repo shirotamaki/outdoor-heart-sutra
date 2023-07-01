33b3ce534e375303fa7c4004212d91e25bceb5a6
shirotamaki <shiro.tamaki63@gmail.com>
2023-07-01 18:24:34 +0900

    ✅  Userのモデルスペックを修正する

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -5,53 +5,53 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "validations" do
     it "validates presence of provider" do
-      user = build(:user, provider: nil)
+      user = FactoryBot.build(:user, provider: nil)
       expect(user).not_to be_valid
     end
 
     it "validates presence of uid" do
-      user = build(:user, uid: nil)
+      user = FactoryBot.build(:user, uid: nil)
       expect(user).not_to be_valid
     end
 
     it "validates uniqueness of uid" do
-      create(:user, uid: "test_uid")
-      user = build(:user, uid: "test_uid")
+      FactoryBot.create(:user, uid: "test_uid")
+      user = FactoryBot.build(:user, uid: "test_uid")
       expect(user).not_to be_valid
     end
 
     it "validates presence of name" do
-      user = build(:user, name: nil)
+      user = FactoryBot.build(:user, name: nil)
       expect(user).not_to be_valid
     end
 
     it "validates length of name" do
-      user = build(:user, name: "a" * 257)
+      user = FactoryBot.build(:user, name: "a" * 257)
       expect(user).not_to be_valid
     end
 
     it "validates presence of email" do
-      user = build(:user, email: nil)
+      user = FactoryBot.build(:user, email: nil)
       expect(user).not_to be_valid
     end
 
     it "validates uniqueness of email" do
-      create(:user, email: "test@example.com")
-      user = build(:user, email: "test@example.com")
+      FactoryBot.create(:user, email: "test@example.com")
+      user = FactoryBot.build(:user, email: "test@example.com")
       expect(user).not_to be_valid
     end
 
     it "validates length of email" do
-      user = build(:user, email: "a" * 243 + "@example.com")
+      user = FactoryBot.build(:user, email: "a" * 257 )
       expect(user).not_to be_valid
     end
   end
 
   describe "associations" do
     it "has many photos" do
-      user = create(:user)
-      create(:photo, user: user)
-      create(:photo, user: user)
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:photo, user: user)
+      FactoryBot.create(:photo, user: user)
       expect(user.photos.count).to eq(2)
     end
   end
