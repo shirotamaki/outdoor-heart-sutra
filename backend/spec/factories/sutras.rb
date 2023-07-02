3e64ae951c67490ba98bd521db5c1c3d30908056
shirotamaki <shiro.tamaki63@gmail.com>
2023-07-02 12:41:22 +0900

    ♻️  Sutraのモデルスペックをリファクタリングする

--- backend/spec/factories/sutras.rb
+++ backend/spec/factories/sutras.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require "csv"
+
 FactoryBot.define do
   factory :sutra do
-    sequence(:kanji) { |n| "sutra#{n}" }
+    sutras = CSV.read(Rails.root.join("db", "sutras_kanji_278_moji.csv")).flatten
+    sequence(:kanji) { |n| sutras[n] }
   end
 end
