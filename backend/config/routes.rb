06caa2d210e9072cc1e9fc60174863db3cc40b4e
shirotamaki <shiro.tamaki63@gmail.com>
2023-07-03 23:00:12 +0900

    🩹 ルーティングを修正する

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'users/find', to: 'users#find_user'
       resources :users, only: [:create, :show]
-      get "photos/find", to: "photos#find_photo_by_sutra_and_user"
+      get 'users/find', to: 'users#find_user'
+      delete "users/:email", to: "users#destroy", constraints: { email: /[^\/]+/ }
+
       resources :sutras, only: [:index, :show]
+
       resources :photos, only: [:create, :show, :update, :destroy]
+      get "photos/find", to: "photos#find_photo_by_sutra_and_user"
       get 'users/:user_id/photos', to: 'photos#index_by_user'
     end
   end
-  delete "users/:email", to: "api/v1/users#destroy", constraints: { email: /[^\/]+/ }
 end
