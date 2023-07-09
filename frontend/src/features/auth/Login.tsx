d14a36c383d5ffaca6c69b2880a118e2b27bb190
shirotamaki <shiro.tamaki63@gmail.com>
2023-07-09 21:11:04 +0900

    ✅ Loginのテストを追加する

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image'
 import { useSession, signIn } from 'next-auth/react'
-import { useState } from 'react'
 
 const Login = () => {
   const { data: session, status } = useSession()
---
+++ frontend/src/features/auth/__tests__/Login.test.tsx
@@ -0,0 +1,49 @@
+import { render, fireEvent } from '@testing-library/react'
+import { useSession, signIn } from 'next-auth/react'
+import Login from '@/features/auth/Login'
+
+jest.mock('next-auth/react')
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      pathname: '/',
+    }
+  },
+}))
+
+describe('Login', () => {
+  let useSessionMock: jest.Mock
+  let signInMock: jest.Mock
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    useSessionMock = useSession as jest.Mock
+    signInMock = signIn as jest.Mock
+  })
+
+  it('should render loading state initially', () => {
+    useSessionMock.mockReturnValue({ status: 'loading' })
+    const { getByText } = render(<Login />)
+    expect(getByText('Loading...')).toBeInTheDocument()
+  })
+
+  it('should render sign in button if not authenticated', () => {
+    useSessionMock.mockReturnValue({ status: 'unauthenticated' })
+    const { getByText } = render(<Login />)
+    expect(getByText('Googleでログインして始める')).toBeInTheDocument()
+  })
+
+  it('should call signIn when sign in button is clicked', () => {
+    useSessionMock.mockReturnValue({ status: 'unauthenticated' })
+    const { getByText } = render(<Login />)
+    const signInButton = getByText('Googleでログインして始める')
+    fireEvent.click(signInButton)
+    expect(signInMock).toHaveBeenCalledWith('google', { prompt: 'login', callbackUrl: '/' })
+  })
+
+  it('should render null if authenticated', () => {
+    useSessionMock.mockReturnValue({ status: 'authenticated' })
+    const { container } = render(<Login />)
+    expect(container.firstChild).toBeNull()
+  })
+})
