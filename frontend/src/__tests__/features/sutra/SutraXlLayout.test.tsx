794420d1257e0cd8912c89b2c865a50606b7ee1d
shirotamaki <shiro.tamaki63@gmail.com>
2023-08-16 15:31:50 +0900

    ✅ プロパティにcreated_atを追加する

--- frontend/src/__tests__/features/sutra/SutraXlLayout.test.tsx
+++ frontend/src/__tests__/features/sutra/SutraXlLayout.test.tsx
@@ -34,6 +34,7 @@ describe('SutraXlLayout', () => {
         latitude: 1,
         image_url: 'http://example.com/imageUrl1',
         cropped_image_url: 'http://example.com/croppedImageUrl1',
+        created_at: 'Sun, 13 Aug 2023 01:02:04.195159000 UTC +00:00',
         user_id: 1,
         sutra_id: 1,
       },
