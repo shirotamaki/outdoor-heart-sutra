bf51a41f21efac25b8937005087a15fad9918568
shirotamaki <shiro.tamaki63@gmail.com>
2023-08-30 18:28:34 +0900

    ♻️ 重複しているコードを削除する

--- frontend/src/features/sutra/SutraList.tsx
+++ frontend/src/features/sutra/SutraList.tsx
@@ -1,73 +1,10 @@
-import axios from 'axios'
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { getSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
-import { railsApiUrl } from '@/config/index'
 import SutraMdLayout from '@/features/sutra/SutraMdLayout'
 import SutraOverXlLayout from '@/features/sutra/SutraOverXlLayout'
 import SutraSmLayout from '@/features/sutra/SutraSmLayout'
 import SutraXlLayout from '@/features/sutra/SutraXlLayout'
-import fetchUserId from '@/features/user/fetchUserId'
 import useWindowWidth from '@/hooks/useWindowWidth'
 import { SutraListProps } from '@/types/types'
-
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext,
-) => {
-  const session = await getSession(context)
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  if (session.user && session.user.email) {
-    const currentUserId = await fetchUserId(session.user.email)
-
-    if (!currentUserId) {
-      return {
-        redirect: {
-          destination: '/',
-          permanent: false,
-        },
-      }
-    }
-
-    try {
-      const sutraResponse = await axios.get(`${railsApiUrl}/api/v1/sutras`)
-      const sutras = sutraResponse.data
-
-      const photoResponse = await axios.get(`${railsApiUrl}/api/v1/users/${currentUserId}/photos`)
-      const photos = photoResponse.data
-
-      return {
-        props: {
-          sutras,
-          photos,
-        },
-      }
-    } catch (error) {
-      console.error(error)
-      return {
-        redirect: {
-          destination: '/error',
-          permanent: false,
-        },
-      }
-    }
-  } else {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-}
 
 const SutraList = ({ sutras, photos }: SutraListProps) => {
   const windowWidth = useWindowWidth()
