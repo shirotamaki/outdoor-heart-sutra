d2c15c5700ae3c1d7c10c3575191719058166cec
shirotamaki <shiro.tamaki63@gmail.com>
2023-08-30 16:14:07 +0900

    ♻️ %i記法に変更する

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :find_photos, only: [:index]
-      resources :find_users, only: [:index]
+      resources :find_photos, only: %i(index)
+      resources :find_users, only: %i(index)
 
-      resources :photos, only: [:create, :show, :update, :destroy]
-      resources :sutras, only: [:index, :show]
+      resources :sutras, only: %i(index show)
+      resources :photos, only: %i(create show update destroy)
 
-      resources :users, only: [:create, :show, :destroy] do
-        resources :photos, only: [:index]
+      resources :users, only: %i(create show destroy) do
+        # 特定のuserが持つ全てのphotoを取得する場合： /api/v1/users/:user_id/photos
+        resources :photos, only: %i(index)
       end
     end
   end
