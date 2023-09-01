e56f79fe45a4c92d84fe2cdf14c203093709ff92
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-01 23:53:08 +0900

    💄 退会ページのデザインを修正する

--- frontend/src/pages/delete-account.tsx
+++ frontend/src/pages/delete-account.tsx
@@ -29,11 +29,11 @@ const DeleteAccount = () => {
       <CustomHead title='退会' ogUrl='https://www.outdoor-heart-sutra.com/delete-account' />
       <Header />
       <main className=' bg-beige flex-grow'>
-        <div className='flex flex-col justify-center items-center m-12'>
-          <h1 className='text-mainBlack font-kinuta text-lg md:text-2xl mb-12'>
+        <div className='flex flex-col justify-center items-center m-4'>
+          <h1 className='text-mainBlack font-kinuta text-lg md:text-2xl m-4 md:m-8'>
             アカウントを削除する
           </h1>
-          <div className='items-left text-mainBlack font-kinuta text-base md:text-xl'>
+          <div className='items-left text-mainBlack font-kinuta text-sm md:text-base'>
             <p className='pb-4'>削除すると以下の情報が全て失われます</p>
             <ul className='list-disc px-6'>
               <li className='pb-2'>写真</li>
@@ -41,7 +41,7 @@ const DeleteAccount = () => {
               <li className='pb-2'>メモ</li>
             </ul>
           </div>
-          <div className='mt-12'>
+          <div className='mt-16 mb-4'>
             <DeleteUser />
           </div>
         </div>
