efab2ad0f4ae4611b74c45e2988f43fe3ac20343
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-01 23:28:11 +0900

    💄 利用規約、プライバシーポリシーのデザインを修正する

--- frontend/src/pages/privacy-policy.tsx
+++ frontend/src/pages/privacy-policy.tsx
@@ -13,22 +13,28 @@ const PrivacyPolicy = () => {
       />
       <Header />
       <main className='bg-beige flex flex-grow justify-center'>
-        <article className='max-w-screen-sm  font-kinuta text-black/50 p-8'>
-          <h1 className='text-2xl mb-4 text-black/50 text-center'>プライバシーポリシー</h1>
-          <p className='mb-8'>
+        <article className='lg:max-w-screen-sm max-w-552 font-kinuta text-gray-500 m-4'>
+          <h1 className='text-lg md:text-2xl m-4 md:m-8 text-mainBlack text-center'>
+            プライバシーポリシー
+          </h1>
+          <p className='text-sm md:text-base mb-4 md:mb-8'>
             「アウトドア般若心経」（以下、「本サービス」といいます。）における、ユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
           </p>
-          <h2 className='text-xl mb-4 text-black/70'>第1条（個人情報）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第1条（個人情報）</h2>
+          <p className='text-sm md:text-base mb-8'>
             「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、生存する個人に関する情報であって、当該情報に含まれる氏名、生年月日、住所、電話番号、連絡先その他の記述等により特定の個人を識別できる情報及び容貌、指紋、声紋にかかるデータ、及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。
           </p>
-          <h2 className='text-xl mb-4 text-black/70'>第2条（個人情報の収集方法）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第2条（個人情報の収集方法）</h2>
+          <p className='text-sm md:text-base mb-8'>
             本サービスは、ユーザーが利用登録をする際にユーザーのGoogleアカウント情報を取得します。
           </p>
-          <h2 className='text-xl mb-4 text-black/70'>第3条（個人情報を収集・利用する目的）</h2>
-          <p className='mb-4'>本サービスが個人情報を収集・利用する目的は、以下のとおりです。</p>
-          <ol className='list-decimal mb-8 px-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第3条（個人情報を収集・利用する目的）
+          </h2>
+          <p className='text-sm md:text-base mb-4'>
+            本サービスが個人情報を収集・利用する目的は、以下のとおりです。
+          </p>
+          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
             <li>本サービスの提供・運営のため</li>
             <li>ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
             <li>メンテナンス、重要なお知らせなど必要に応じたご連絡のため</li>
@@ -40,11 +46,8 @@ const PrivacyPolicy = () => {
             </li>
             <li>上記の利用目的に付随する目的</li>
           </ol>
-          <h2 className='text-xl mb-4 text-black/70'>第4条（利用目的の変更）</h2>
-          <ol
-            className='list-decimal mb-8
-          px-6'
-          >
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第4条（利用目的の変更）</h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
             <li>
               本サービスは、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。
             </li>
@@ -52,11 +55,13 @@ const PrivacyPolicy = () => {
               利用目的の変更を行った場合には、変更後の目的について、本サービス所定の方法によりユーザーに通知し、または本ウェブサイト上に公表するものとします。
             </li>
           </ol>
-          <h2 className='text-xl mb-4 text-black/70'>第5条（個人情報の第三者提供）</h2>
-          <ol className='list-decimal mb-8 px-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第5条（個人情報の第三者提供）
+          </h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
             <li>
               本サービスは、次に掲げる場合を除いて、あらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。ただし、個人情報保護法その他の法令で認められる場合を除きます。
-              <ol className='list-decimal px-6 py-4'>
+              <ol className='text-sm md:text-base list-decimal px-6 py-4'>
                 <li>
                   人の生命、身体または財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき
                 </li>
@@ -68,7 +73,7 @@ const PrivacyPolicy = () => {
                 </li>
                 <li>
                   予め次の事項を告知あるいは公表し、かつ本サービスが個人情報保護委員会に届出をしたとき
-                  <ol className='list-decimal px-6 pt-4'>
+                  <ol className='text-sm md:text-base list-decimal px-6 pt-4'>
                     <li>利用目的に第三者への提供を含むこと</li>
                     <li>第三者に提供されるデータの項目</li>
                     <li>第三者への提供の手段または方法</li>
@@ -80,7 +85,7 @@ const PrivacyPolicy = () => {
             </li>
             <li>
               前項の定めにかかわらず、次に掲げる場合には、当該情報の提供先は第三者に該当しないものとします。
-              <ol className='list-decimal px-6 pt-4'>
+              <ol className='text-sm md:text-base list-decimal px-6 pt-4'>
                 <li>
                   本サービスが利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合
                 </li>
@@ -91,12 +96,14 @@ const PrivacyPolicy = () => {
               </ol>
             </li>
           </ol>
-          <h2 className='text-xl mb-4 text-black/70'>第6条（退会時）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第6条（退会時）</h2>
+          <p className='text-sm md:text-base mb-8'>
             ユーザーが本サービスの退会手続きを行った場合、本サービスはユーザーの個人情報を全て削除します。
           </p>
-          <h2 className='text-xl mb-4 text-black/70'>第7条（プライバシーポリシーの変更）</h2>
-          <ol className='list-decimal mb-8 px-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第7条（プライバシーポリシーの変更）
+          </h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
             <li>
               本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく、変更することができるものとします。
             </li>
@@ -105,17 +112,19 @@ const PrivacyPolicy = () => {
             </li>
           </ol>
           <h2
-            className='text-xl mb-4
-          text-black/70'
+            className='text-base md:text-xl mb-4
+          text-mainBlack'
           >
             第8条（お問い合わせ窓口）
           </h2>
-          <p className='mb-1'>本ポリシーに関するお問い合わせは，下記の窓口までお願いいたします。</p>
-          <p>
+          <p className='text-sm md:text-base mb-1'>
+            本ポリシーに関するお問い合わせは，下記の窓口までお願いいたします。
+          </p>
+          <p className='mb-4 md:mb-8'>
             <Image src='/images/email_address.png' alt='メールアドレス' width={300} height={24} />
           </p>
-          <p className='mb-8 text-right'>以上</p>
-          <p className='mb-8 text-center'>
+          <p className='text-sm md:text-base text-right mb-4 md:mb-8'>以上</p>
+          <p className='text-sm md:text-base mb-4 md:mb-8'>
             <Link href='/' className='hover:opacity-50 transition-all duration-100'>
               トップページに戻る
             </Link>
--- frontend/src/pages/terms-of-service.tsx
+++ frontend/src/pages/terms-of-service.tsx
@@ -9,13 +9,13 @@ const TermsOfService = () => {
       <CustomHead title='利用規約' ogUrl='https://www.outdoor-heart-sutra.com/terms-of-service' />
       <Header />
       <main className='bg-beige flex flex-grow justify-center'>
-        <article className='max-w-screen-sm  font-kinuta text-black/50 p-8'>
-          <h1 className='text-2xl mb-4 text-black/50 text-center'>利用規約</h1>
-          <p className='mb-8'>
+        <article className='lg:max-w-screen-sm max-w-552 font-kinuta text-gray-500 m-4'>
+          <h1 className='text-lg md:text-2xl m-4 md:m-8 text-mainBlack text-center'>利用規約</h1>
+          <p className='text-sm md:text-base mb-4 md:mb-8'>
             この利用規約（以下、「本規約」といいます。）は、このウェブサイト上で提供するサービス（以下、「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下、「ユーザー」といいます。）には、本規約に従って、本サービスをご利用いただきます。
           </p>
-          <h2 className='text-xl mb-4 text-black/70'>第1条（適用）</h2>
-          <ol className='list-decimal mb-6 mx-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第1条（適用）</h2>
+          <ol className='text-sm md:text-base list-decimal mb-6 mx-6'>
             <li>
               本規約は、ユーザーと本サービスの利用に関わる一切の関係に適用されるものとします。
             </li>
@@ -27,14 +27,14 @@ const TermsOfService = () => {
             </li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第2条（利用登録）</h2>
-          <ol className='list-decimal mb-8 px-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第2条（利用登録）</h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
             <li>
               本サービスの利用登録は、登録希望者が本規約に同意の上、本サービスの定める方法により利用登録をすることで完了するものとします。
             </li>
             <li>
               本サービスは、利用登録の申請者に以下の事由があると判断した場合、利用登録の申請を承認しないことがあり、その理由については一切の開示義務を負わないものとします。
-              <ol className='list-decimal my-4 px-6'>
+              <ol className='text-sm md:text-base list-decimal my-4 px-6'>
                 <li>利用登録の申請に際して虚偽の事項を届け出た場合</li>
                 <li>本規約に違反したことがある者からの申請である場合</li>
                 <li>その他、本サービスが利用登録を相当でないと判断した場合</li>
@@ -42,8 +42,8 @@ const TermsOfService = () => {
             </li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第3条（ユーザー情報の管理）</h2>
-          <ol className='list-decimal mb-8 mx-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第3条（ユーザー情報の管理）</h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 mx-6'>
             <li>
               ユーザーは、自己の責任において、本サービスのユーザー情報を適切に管理するものとします。
             </li>
@@ -55,16 +55,16 @@ const TermsOfService = () => {
             </li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第4条（利用料金）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第4条（利用料金）</h2>
+          <p className='text-sm md:text-base mb-8'>
             ユーザーは、本サービスを無料で利用することができます。本サービス内で利用料金を請求することはありません。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第5条（禁止事項）</h2>
-          <p className='mb-4'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第5条（禁止事項）</h2>
+          <p className='text-sm md:text-base mb-4'>
             ユーザーは、本サービスの利用にあたり、以下の行為をしてはなりません。
           </p>
-          <ol className='list-decimal mb-8 mx-6'>
+          <ol className='text-sm md:text-base list-decimal mb-8 mx-6'>
             <li>法令または公序良俗に違反する行為</li>
             <li>犯罪行為に関連する行為</li>
             <li>
@@ -86,11 +86,13 @@ const TermsOfService = () => {
             <li>その他、本サービスが不適切と判断する行為</li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第6条（本サービスの提供の停止等）</h2>
-          <ol className='list-decimal mb-8 mx-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第6条（本サービスの提供の停止等）
+          </h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 mx-6'>
             <li>
               本サービスは、以下のいずれかの事由があると判断した場合、ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
-              <ol className='list-decimal my-4 mx-6'>
+              <ol className='text-sm md:text-base list-decimal my-4 mx-6'>
                 <li>本サービスにかかるコンピュータシステムの保守点検または更新を行う場合</li>
                 <li>
                   地震、落雷、火災、停電または天災などの不可抗力により、本サービスの提供が困難となった場合
@@ -104,11 +106,13 @@ const TermsOfService = () => {
             </li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第7条（利用制限および登録抹消）</h2>
-          <ol className='list-decimal mb-8 mx-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第7条（利用制限および登録抹消）
+          </h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 mx-6'>
             <li>
               本サービスは、ユーザーが以下のいずれかに該当する場合には、事前の通知なく、ユーザーに対して、本サービスの全部もしくは一部の利用を制限し、またはユーザーとしての登録を抹消することができるものとします。
-              <ol className='list-decimal my-4 mx-6'>
+              <ol className='text-sm md:text-base list-decimal my-4 mx-6'>
                 <li>本規約のいずれかの条項に違反した場合</li>
                 <li>登録事項に虚偽の事実があることが判明した場合</li>
                 <li>料金等の支払債務の不履行があった場合</li>
@@ -122,13 +126,15 @@ const TermsOfService = () => {
             </li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第8条（退会）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第8条（退会）</h2>
+          <p className='text-sm md:text-base mb-8'>
             ユーザーは、本サービスの定める退会手続により、本サービスから退会できるものとします。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第9条（保証の否認および免責事項）</h2>
-          <ol className='list-decimal mb-8 mx-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第9条（保証の否認および免責事項）
+          </h2>
+          <ol className='text-sm md:text-base list-decimal mb-8 mx-6'>
             <li>
               本サービスは、本サービスに事実上または法律上の瑕疵（安全性、信頼性、正確性、完全性、有効性、特定の目的への適合性、セキュリティなどに関する欠陥、エラーやバグ、権利侵害などを含みます。）がないことを明示的にも黙示的にも保証しておりません。
             </li>
@@ -143,40 +149,44 @@ const TermsOfService = () => {
             </li>
           </ol>
 
-          <h2 className='text-xl mb-4 text-black/70'>第10条（サービス内容の変更等）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第10条（サービス内容の変更等）
+          </h2>
+          <p className='text-sm md:text-base mb-8'>
             本サービスは、ユーザーへの事前の告知をもって、本サービスの内容を変更、追加または廃止することがあり、ユーザーはこれを承諾するものとします。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第11条（利用規約の変更）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第11条（利用規約の変更）</h2>
+          <p className='text-sm md:text-base mb-8'>
             本サービスは、必要と判断した場合には、ユーザーに通知することなくいつでも本規約を変更することができるものとします。なお、本規約の変更後、本サービスの利用を開始した場合には、当該ユーザーは変更後の規約に同意したものとみなします。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第12条（個人情報の取扱い）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第12条（個人情報の取扱い）</h2>
+          <p className='text-sm md:text-base mb-8'>
             本サービスの利用によって取得する個人情報については、本サービスの「プライバシーポリシー」に従い適切に取り扱うものとします。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第13条（通知または連絡）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第13条（通知または連絡）</h2>
+          <p className='text-sm md:text-base mb-8'>
             ユーザーと本サービスとの間の通知または連絡は、本サービスの定める方法によって行うものとします。本サービスは、ユーザーから、本サービスが別途定める方式に従った変更届け出がない限り、現在登録されている連絡先が有効なものとみなして当該連絡先へ通知または連絡を行い、これらは、発信時にユーザーへ到達したものとみなします。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第14条（権利義務の譲渡の禁止）</h2>
-          <p className='mb-8'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
+            第14条（権利義務の譲渡の禁止）
+          </h2>
+          <p className='text-sm md:text-base mb-8'>
             ユーザーは、本サービスの書面による事前の承諾なく、利用契約上の地位または本規約に基づく権利もしくは義務を第三者に譲渡し、または担保に供することはできません。
           </p>
 
-          <h2 className='text-xl mb-4 text-black/70'>第15条（準拠法・裁判管轄）</h2>
-          <ol className='list-decimal mb-8 mx-6'>
+          <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第15条（準拠法・裁判管轄）</h2>
+          <ol className='text-sm md:text-base list-decimal mx-6 mb-4 md:mb-8'>
             <li>本規約の解釈にあたっては、日本法を準拠法とします。</li>
             <li>
               本サービスに関して紛争が生じた場合には、本サービスの本店所在地を管轄する裁判所を専属的合意管轄とします。
             </li>
           </ol>
-          <p className='mb-8 text-right'>以上</p>
-          <p className='mb-8 text-center'>
+          <p className='text-sm md:text-base text-right mb-4 md:mb-8'>以上</p>
+          <p className='text-sm md:text-base mb-4 md:mb-8'>
             <Link href='/' className='hover:opacity-50 transition-all duration-100'>
               トップページに戻る
             </Link>
