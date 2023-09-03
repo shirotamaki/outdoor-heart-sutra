df7a26b422b5fedfbafbeab689d3cbd690a74ce9
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-03 18:22:40 +0900

    💄 mypageのユーザ名、出家日のグリッドを揃える実装を修正する

--- frontend/src/pages/mypage.tsx
+++ frontend/src/pages/mypage.tsx
@@ -61,11 +61,13 @@ const MyPage = ({ photos, user }: MyPageProps) => {
               style={{ borderRadius: '50px' }}
             />
           </h1>
-          <div className='grid grid-rows-2 grid-cols-2 font-kinuta text-mainBlack text-base md:text-xl mb-12 pr-20'>
-            <div className='justify-self-end'>ユーザ名：</div>
-            <div className='justify-self-start' data-testid="user-name">{user.name}</div>
-            <div className='justify-self-end'>出家日：</div>
-            <div className='justify-self-start'>{formattedDate}</div>
+          <div className='grid grid-rows-2 grid-cols-3 font-kinuta text-mainBlack text-base md:text-xl mb-12'>
+            <div className='justify-self-end col-span-1'>ユーザ名：</div>
+            <div className='justify-self-start col-span-2' data-testid='user-name'>
+              {user.name}
+            </div>
+            <div className='justify-self-end col-span-1'>出家日：</div>
+            <div className='justify-self-start col-span-2'>{formattedDate}</div>
           </div>
           <div className='font-kinuta text-mainBlack text-base md:text-xl mb-12 flex flex-col justify-center items-center'>
             <div>写経した総数</div>
