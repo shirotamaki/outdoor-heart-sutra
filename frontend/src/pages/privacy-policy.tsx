9864f85e90cf3dbbf2eef3d232ec670dd12ecead
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-10 22:13:40 +0900

    💄 line-height(leading)を追加する

--- frontend/src/pages/privacy-policy.tsx
+++ frontend/src/pages/privacy-policy.tsx
@@ -17,24 +17,24 @@ const PrivacyPolicy = () => {
           <h1 className='text-lg md:text-2xl m-4 md:m-8 text-mainBlack text-center'>
             プライバシーポリシー
           </h1>
-          <p className='text-sm md:text-base mb-4 md:mb-8'>
+          <p className='text-xs md:text-base mb-4 md:mb-8 leading-6 md:leading-8'>
             「アウトドア般若心経」（以下、「本サービス」といいます。）における、ユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
           </p>
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第1条（個人情報）</h2>
-          <p className='text-sm md:text-base mb-8'>
+          <p className='text-xs md:text-base mb-8 leading-6 md:leading-8'>
             「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、生存する個人に関する情報であって、当該情報に含まれる氏名、生年月日、住所、電話番号、連絡先その他の記述等により特定の個人を識別できる情報及び容貌、指紋、声紋にかかるデータ、及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。
           </p>
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第2条（個人情報の収集方法）</h2>
-          <p className='text-sm md:text-base mb-8'>
+          <p className='text-xs md:text-base mb-8 leading-6 md:leading-8'>
             本サービスは、ユーザーが利用登録をする際にユーザーのGoogleアカウント情報を取得します。
           </p>
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
             第3条（個人情報を収集・利用する目的）
           </h2>
-          <p className='text-sm md:text-base mb-4'>
+          <p className='text-xs md:text-base mb-4 leading-6 md:leading-8'>
             本サービスが個人情報を収集・利用する目的は、以下のとおりです。
           </p>
-          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
+          <ol className='text-xs md:text-base list-decimal mb-8 px-6 leading-6 md:leading-8'>
             <li>本サービスの提供・運営のため</li>
             <li>ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
             <li>メンテナンス、重要なお知らせなど必要に応じたご連絡のため</li>
@@ -47,7 +47,7 @@ const PrivacyPolicy = () => {
             <li>上記の利用目的に付随する目的</li>
           </ol>
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第4条（利用目的の変更）</h2>
-          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
+          <ol className='text-xs md:text-base list-decimal mb-8 px-6 leading-6 md:leading-8'>
             <li>
               本サービスは、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。
             </li>
@@ -58,10 +58,10 @@ const PrivacyPolicy = () => {
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
             第5条（個人情報の第三者提供）
           </h2>
-          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
+          <ol className='text-xs md:text-base list-decimal mb-8 px-6 leading-6 md:leading-8'>
             <li>
               本サービスは、次に掲げる場合を除いて、あらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。ただし、個人情報保護法その他の法令で認められる場合を除きます。
-              <ol className='text-sm md:text-base list-decimal px-6 py-4'>
+              <ol className='text-xs md:text-base list-decimal px-6 py-4 leading-6 md:leading-8'>
                 <li>
                   人の生命、身体または財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき
                 </li>
@@ -73,7 +73,7 @@ const PrivacyPolicy = () => {
                 </li>
                 <li>
                   予め次の事項を告知あるいは公表し、かつ本サービスが個人情報保護委員会に届出をしたとき
-                  <ol className='text-sm md:text-base list-decimal px-6 pt-4'>
+                  <ol className='text-xs md:text-base list-decimal px-6 pt-4 leading-6 md:leading-8'>
                     <li>利用目的に第三者への提供を含むこと</li>
                     <li>第三者に提供されるデータの項目</li>
                     <li>第三者への提供の手段または方法</li>
@@ -85,7 +85,7 @@ const PrivacyPolicy = () => {
             </li>
             <li>
               前項の定めにかかわらず、次に掲げる場合には、当該情報の提供先は第三者に該当しないものとします。
-              <ol className='text-sm md:text-base list-decimal px-6 pt-4'>
+              <ol className='text-xs md:text-base list-decimal px-6 pt-4 leading-6 md:leading-8'>
                 <li>
                   本サービスが利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合
                 </li>
@@ -97,13 +97,13 @@ const PrivacyPolicy = () => {
             </li>
           </ol>
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>第6条（退会時）</h2>
-          <p className='text-sm md:text-base mb-8'>
+          <p className='text-xs md:text-base mb-8 leading-6 md:leading-8'>
             ユーザーが本サービスの退会手続きを行った場合、本サービスはユーザーの個人情報を全て削除します。
           </p>
           <h2 className='text-base md:text-xl mb-4 text-mainBlack'>
             第7条（プライバシーポリシーの変更）
           </h2>
-          <ol className='text-sm md:text-base list-decimal mb-8 px-6'>
+          <ol className='text-xs md:text-base list-decimal mb-8 px-6 leading-6 md:leading-8'>
             <li>
               本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく、変更することができるものとします。
             </li>
@@ -117,14 +117,14 @@ const PrivacyPolicy = () => {
           >
             第8条（お問い合わせ窓口）
           </h2>
-          <p className='text-sm md:text-base mb-1'>
+          <p className='text-xs md:text-base mb-1 leading-6 md:leading-8'>
             本ポリシーに関するお問い合わせは，下記の窓口までお願いいたします。
           </p>
           <p className='mb-4 md:mb-8'>
             <Image src='/images/email_address.png' alt='Email Adress' width={300} height={24} />
           </p>
-          <p className='text-sm md:text-base text-right mb-4 md:mb-8'>以上</p>
-          <p className='text-sm md:text-base mb-4 md:mb-8'>
+          <p className='text-xs md:text-base text-right mb-4 md:mb-8'>以上</p>
+          <p className='text-center text-sm md:text-lg mb-4 md:mb-8'>
             <Link href='/' className='hover:opacity-50 transition-all duration-100'>
               トップページに戻る
             </Link>
