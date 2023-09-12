f7e153bf100aed59fbbe36abc72589c664e9f538
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-12 23:57:40 +0900

    ♻️ moduleオプションを利用しルーティングの構成を修正する

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :sutras, only: %i(index show)
       resources :photos, only: %i(index create show update destroy)
-
       resources :users, only: %i(index create show destroy) do
-        # 特定のuserが持つ全てのphotoを取得する場合： /api/v1/users/:user_id/photos
-        resources :photos, only: %i(index)
+        resources :photos, only: %i(index), module: :users
       end
     end
   end
