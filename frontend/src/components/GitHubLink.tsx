6dfbfd8cb46c80af548cba525ce52d719076def2
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-13 23:27:32 +0900

    ✨ Fotterに配置している外部リンクに`target='_blank'`を設定する
    Linkコンポーネントに<a>タグを指定するとエラーとなるため、legacyBehavior プロパティを設定する

--- frontend/src/components/GitHubLink.tsx
+++ frontend/src/components/GitHubLink.tsx
@@ -3,19 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
 
 const GitHubLink = () => (
-  <Link
-    href='https://github.com/shirotamaki/outdoor-heart-sutra'
-    className='hover:opacity-50 transition-all duration-100'
-    data-testid='github-link'
-  >
-    <FontAwesomeIcon
-      icon={faGithub}
-      style={{
-        width: '18px',
-        height: '18px',
-      }}
-      aria-label='GitHub Icon'
-    />
+  <Link href='https://github.com/shirotamaki/outdoor-heart-sutra' legacyBehavior>
+    <a
+      target='_blank'
+      rel='noopener noreferrer'
+      className='hover:opacity-50 transition-all duration-100'
+      data-testid='github-link'
+    >
+      <FontAwesomeIcon
+        icon={faGithub}
+        style={{
+          width: '18px',
+          height: '18px',
+        }}
+        aria-label='GitHub Icon'
+      />
+    </a>
   </Link>
 )
 
