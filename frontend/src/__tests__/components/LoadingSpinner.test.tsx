ecd93b9dafb888068b6afeecc33bdf2f269c0af4
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-17 21:10:34 +0900

    ✅ LoadingSpinnerのテストを追加する

---
+++ frontend/src/__tests__/components/LoadingSpinner.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+import LoadingSpinner from '@/components/LoadingSpinner'
+
+describe('LoadingSpinner Component', () => {
+  it('renders Loading Spinner', () => {
+    const { getByLabelText } = render(<LoadingSpinner />)
+
+    const loadingSpinner = getByLabelText('Loading Spinner')
+    expect(loadingSpinner).toBeInTheDocument()
+
+  })
+})
