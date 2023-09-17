703aba80c5c4b6a7b519f79f20815f555b468f3e
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-17 21:32:12 +0900

    🩹 Spinnerの表示位置を調整する

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
   return (
-    <div className='flex justify-center items-center h-48 w-screen' aria-label='Loading Spinner'>
+    <div className='flex justify-center items-center h-80 w-screen' aria-label='Loading Spinner'>
       <div className='animate-spin h-12 w-12 border-4 border-blue-800 rounded-full border-t-transparent'></div>
     </div>
   )
