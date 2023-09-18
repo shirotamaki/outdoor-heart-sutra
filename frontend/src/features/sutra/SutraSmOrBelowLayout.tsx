6c120d4331411acbedd84a3ed0f68224c014d21d
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-18 22:07:28 +0900

    ♻️ :racehorse: Object.fromEntriesを利用しパフォーマンスを改善する
    renderSutraLayout関数を用意しDRYにする

--- frontend/src/features/sutra/SutraSmOrBelowLayout.tsx
+++ frontend/src/features/sutra/SutraSmOrBelowLayout.tsx
@@ -9,93 +9,49 @@ const SutraSmOrBelowLayout = ({ sutras, photos }: { sutras: Sutra[]; photos: Pho
   const fourthLayout = sutras.slice(180, 240)
   const fifthLayout = sutras.slice(240, 279)
 
+  const photoMap: { [key: number]: Photo } = Object.fromEntries(
+    photos.map((photo) => [photo.sutra_id, photo]),
+  )
+
   return (
     <div>
       <div className='font-reggae text-mainBlack flex justify-end mb-2'>
         <SutraCalculator photos={photos} />
       </div>
       <div className='vertical-sutras-container grid gap-2 grid-cols-6 mb-12'>
-        {firstLayout.map((sutra, index) => {
-          const correspondingPhoto = photos.find((photo) => photo.sutra_id === sutra.id)
-
-          return (
-            <SutraItem
-              key={index}
-              sutraId={sutra.id}
-              sutraKanji={sutra.kanji}
-              correspondingPhoto={correspondingPhoto}
-              width={48}
-              height={48}
-            />
-          )
-        })}
+        {renderSutraLayout(firstLayout, photoMap)}
       </div>
       <div className='vertical-sutras-container grid gap-2 grid-cols-6 mb-12'>
-        {secondLayout.map((sutra, index) => {
-          const correspondingPhoto = photos.find((photo) => photo.sutra_id === sutra.id)
-
-          return (
-            <SutraItem
-              key={index}
-              sutraId={sutra.id}
-              sutraKanji={sutra.kanji}
-              correspondingPhoto={correspondingPhoto}
-              width={48}
-              height={48}
-            />
-          )
-        })}
+        {renderSutraLayout(secondLayout, photoMap)}
       </div>
       <div className='vertical-sutras-container grid gap-2 grid-cols-6 mb-12'>
-        {thirdLayout.map((sutra, index) => {
-          const correspondingPhoto = photos.find((photo) => photo.sutra_id === sutra.id)
-
-          return (
-            <SutraItem
-              key={index}
-              sutraId={sutra.id}
-              sutraKanji={sutra.kanji}
-              correspondingPhoto={correspondingPhoto}
-              width={48}
-              height={48}
-            />
-          )
-        })}
+        {renderSutraLayout(thirdLayout, photoMap)}
       </div>
       <div className='vertical-sutras-container grid gap-2 grid-cols-6 mb-12'>
-        {fourthLayout.map((sutra, index) => {
-          const correspondingPhoto = photos.find((photo) => photo.sutra_id === sutra.id)
-
-          return (
-            <SutraItem
-              key={index}
-              sutraId={sutra.id}
-              sutraKanji={sutra.kanji}
-              correspondingPhoto={correspondingPhoto}
-              width={48}
-              height={48}
-            />
-          )
-        })}
+        {renderSutraLayout(fourthLayout, photoMap)}
       </div>
       <div className='vertical-sutras-container grid gap-2 grid-cols-6 mb-12'>
-        {fifthLayout.map((sutra, index) => {
-          const correspondingPhoto = photos.find((photo) => photo.sutra_id === sutra.id)
-
-          return (
-            <SutraItem
-              key={index}
-              sutraId={sutra.id}
-              sutraKanji={sutra.kanji}
-              correspondingPhoto={correspondingPhoto}
-              width={48}
-              height={48}
-            />
-          )
-        })}
+        {renderSutraLayout(fifthLayout, photoMap)}
       </div>
     </div>
   )
+}
+
+const renderSutraLayout = (sutraLayout: Sutra[], photoMap: { [key: number]: Photo }) => {
+  return sutraLayout.map((sutra) => {
+    const correspondingPhoto = photoMap[sutra.id]
+
+    return (
+      <SutraItem
+        key={sutra.id}
+        sutraId={sutra.id}
+        sutraKanji={sutra.kanji}
+        correspondingPhoto={correspondingPhoto}
+        width={48}
+        height={48}
+      />
+    )
+  })
 }
 
 export default SutraSmOrBelowLayout
