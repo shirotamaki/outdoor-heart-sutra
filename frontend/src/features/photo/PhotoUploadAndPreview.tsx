28b96898ce752608f5e1d9c0fdf9d6b09761a9f6
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-18 22:53:34 +0900

    ♻️ 早期returnに置き換える

--- frontend/src/features/photo/PhotoUploadAndPreview.tsx
+++ frontend/src/features/photo/PhotoUploadAndPreview.tsx
@@ -53,34 +53,33 @@ const PhotoUploadAndPreview = ({ sutraId, photoId, sutra }: PhotoUploadAndPrevie
     setIsLoading(true)
     setSelectedImage(true)
 
-    if (event.target.files !== null) {
-      const file = event.target.files[0]
-
-      await fetchLocation({ file })
-
-      try {
-        const heic2any = (await import('heic2any')).default
-        if (file.type === 'image/heic') {
-          const convertedImage = await heic2any({
-            blob: file,
-            toType: 'image/jpeg',
-            quality: 0.1,
-          })
-          if (Array.isArray(convertedImage)) {
-            throw new Error('Unexpected multiple blobs')
-          }
-
-          setPreviewUrl(URL.createObjectURL(convertedImage))
-          setOriginalBlob(convertedImage)
-        } else {
-          setPreviewUrl(URL.createObjectURL(file))
-          setOriginalBlob(file)
-        }
-      } catch (error) {
-        console.error('Error converting image: ', error)
-      }
-    } else {
+    if (!event.target.files) {
       return
+    }
+
+    const file = event.target.files[0]
+
+    await fetchLocation({ file })
+
+    try {
+      const heic2any = (await import('heic2any')).default
+      if (file.type === 'image/heic') {
+        const convertedImage = await heic2any({
+          blob: file,
+          toType: 'image/jpeg',
+          quality: 0.1,
+        })
+        if (Array.isArray(convertedImage)) {
+          throw new Error('Unexpected multiple blobs')
+        }
+        setPreviewUrl(URL.createObjectURL(convertedImage))
+        setOriginalBlob(convertedImage)
+      } else {
+        setPreviewUrl(URL.createObjectURL(file))
+        setOriginalBlob(file)
+      }
+    } catch (error) {
+      console.error('Error converting image: ', error)
     }
     setIsLoading(false)
   }
