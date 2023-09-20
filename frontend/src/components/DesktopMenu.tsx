3c8e9177ad26d52ee988092c638a5703d6d2e92f
shirotamaki <shiro.tamaki63@gmail.com>
2023-09-20 15:40:01 +0900

    ⚡️ メニューに使い方ページを追加する

--- frontend/src/components/DesktopMenu.tsx
+++ frontend/src/components/DesktopMenu.tsx
@@ -2,9 +2,10 @@ import Menu from '@/components/Menu'
 import Logout from '@/features/auth/Logout'
 
 const DesktopMenu = () => (
-  <div className='text-base space-x-8 flex-row' data-testid='desktop-menu-link'>
-    <Menu href='/maps'>全体地図</Menu>
+  <div className='lg:text-base text-sm lg:space-x-8 space-x-6 flex-row' data-testid='desktop-menu-link'>
+    <Menu href='/maps'>地図</Menu>
     <Menu href='/about'>説明</Menu>
+    <Menu href='/how-to-use'>使い方</Menu>
     <Menu href='/mypage'>マイページ</Menu>
     <Logout />
   </div>
