d63318e3eaad82c60890a30cb2bb6493ee66c376
shirotamaki <shiro.tamaki63@gmail.com>
2023-10-09 23:41:33 +0900

    🩹 テキストを修正する

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
             href='/faq'
             className='hover:opacity-50 transition-all duration-100'
           >
-            FAQ
+            よくある質問
           </Link>
         </nav>
         <nav className='flex flex-row space-x-6 items-center md:mb-3 mb-2'>
